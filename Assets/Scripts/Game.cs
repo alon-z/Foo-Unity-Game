d6b74b04c1feffda5613dca157b8c35bc9de98ce
alonikomax <aloniko.zus@gmail.com>
2016-05-23 01:07:59 +0300

    Added the project V0.0.1

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public Object Enemy;
+    public Rigidbody Player;
+    private ArrayList Enemies;
+
+	// Use this for initialization
+	void Start () {
+        Enemies = new ArrayList();
+
+        Vector3 mainV = new Vector3(0, 0.5f, 5);
+        Enemies.Add(Instantiate(Enemy, mainV, Quaternion.identity) as GameObject);
+    }
+	
+	// Update is 100% called
+	void FixedUpdate () {
+
+        for (int index = 0; index < Enemies.Count; index++)
+        {
+            GameObject enemy = (GameObject)(Enemies[index]);
+            Vector3 movement = new Vector3(0.0f, 0.0f, -0.8f);
+            enemy.GetComponent<Rigidbody>().AddForce(movement);
+            if (Vector3.Distance(enemy.GetComponent<Rigidbody>().position, Player.position) > 18.0f)
+            {
+                Destroy(enemy);
+                Enemies.Remove(enemy);
+                index--;
+                Vector3 mainV = new Vector3(0, 0.5f, 5);
+                Enemies.Add(Instantiate(Enemy, mainV, Quaternion.identity) as GameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public int SPEED;
+    public Rigidbody player;
+
+    private Rect leftZone = new Rect(0, 0, Screen.width / 2, Screen.height);
+    private Rect rightZone = new Rect(Screen.width / 2, 0, Screen.width, Screen.height);
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 100% Sure updates
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            //float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3();
+
+            if (leftZone.Contains(Input.GetTouch(0).position)) {
+                movement = new Vector3(-1.0f, 0.0f, 0.0f);
+            }
+
+            if (rightZone.Contains(Input.GetTouch(0).position))
+            {
+                movement = new Vector3(1.0f, 0.0f, 0.0f);
+            }
+
+            player.AddForce(movement * SPEED);
+        }
+    }
+}
